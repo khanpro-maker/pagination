b1a94140d68657da174c3b2fc92f77d85941be39
farhan khan <farhankhanpro3@gmail.com>
2024-11-10 15:01:50 +0530

    add pages

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 const Pagination = ({pageNo,setPageNo}) => {
+    function handleNext(){
+        setPageNo(pageNo+1)
+}
+    function handlePrev(){
+        setPageNo(pageNo-1)
+    }
   return (
     <>
         <div className='pagination-container'>
-           <div className='page-btn'>{"<"}</div>
+           {pageNo>1? <div onClick={handlePrev} className='page-btn'>{"<"}</div>:""}
            <div className='page-btn'>{pageNo}</div>
-           <div className='page-btn'>{">"}</div>
+           <div onClick={handleNext} className='page-btn'>{">"}</div>
         </div>
     </>
   )
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,13 +10,13 @@ const Post = () => {
     useEffect( ()=>{
          axios.get(`https://picsum.photos/v2/list?page=${pageNo}&limit=5`)
         .then((res)=>setData(res.data))
-    },[])
+    },[pageNo])
   return (
     <div className='container'>
       i am post
       <div className='post-container'>
         {data.map((item,index)=>{
-          return (<img src={item.download_url} />)
+          return (<img key={index} src={item.download_url} />)
         })}
       </div>
       <Pagination pageNo={pageNo} setPageNo={setPageNo}/>

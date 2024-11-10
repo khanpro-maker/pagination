c12147f35c984a304da45e6023ee8452642f155d
farhan khan <farhankhanpro3@gmail.com>
2024-11-10 14:24:21 +0530

    updated

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pagination = () => {
+  return (
+    <>
+        <div>
+            i am Pagination
+        </div>
+    </>
+  )
+}
+
+export default Pagination
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import Pagination from './Pagination'
+import "../index.css"
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
 const Post = () => {
+    const  [data,setData]  = useState([]);
+    useEffect( ()=>{
+         axios.get(`https://picsum.photos/v2/list?page=1&limit=5`)
+        .then((res)=>setData(res.data))
+    },[])
   return (
-    <div>
+    <div className='container'>
       i am post
+      <div className='post-container'>
+        {data.map((item,index)=>{
+          return (<img src={item.download_url} />)
+        })}
+      </div>
+      <Pagination/>
     </div>
   )
 }
